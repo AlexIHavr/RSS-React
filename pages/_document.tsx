0693fe2d8d82c5ef59c52c1123416cdaa5143f9e
alexihavr <dchavronin@bk.ru>
2024-07-23 12:37:03 +0300

    feat: add next init

---
+++ pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+import React from 'react';
+
+export default function Document(): JSX.Element {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
